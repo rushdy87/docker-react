88107a113368e872dc728a502cadbb5a3fac6efd
rushdy87 <mohammed_alrushdy@yahoo.com>
2025-01-25 04:10:24 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello there..</h1>
+      <h2>My name is Mohammed Alrushdy</h2>
+    </div>
+  );
+}
+
+export default App;
